66b52efee0704a10d81437d4aa75cee3884d67d1
Kochnov Andrey <a.kochnov@corp.mail.ru>
2020-03-19 19:25:21 +0300

    sortOrders done

--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, sortByItemNames, sortByDate} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
@@ -20,5 +20,167 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('orders\' types are not object', () => {
+		const resutl = sortByItemCount(1, 2);
+		expect(resutl).toBe(0)
+	})
+
+	it('1 greater than 2', () =>  {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['item4', 'item5'],
+		};
+		const result = sortByItemCount(order1, order2)
+		expect(result).toBe(1)
+	})
+
+	it('1 less than 2', () =>  {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['item3', 'item4', 'item5'],
+		};
+		const result = sortByItemCount(order1, order2)
+		expect(result).toBe(-1)
+	})
+
 });
 
+// describe('sortByItem')
+describe('sortByItemNames function', () => {
+	it('orders\' types are not objects', () => {
+		const result = sortByItemNames(1, 2);
+		expect(result).toBe(0)
+	})
+
+	it('orders are null', () => {
+		const result = sortByItemNames(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders does not contain items', () => {
+		const order1 = {
+			list: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			list: ['1', '2'],
+		};
+		const result = sortByItemNames(order1, order2);
+		expect(result).toEqual(0);
+	});
+
+
+	it('same items names', () => {
+		const order1 = {
+			items: ['a', 'b', 'c'],
+		};
+
+		const order2 = {
+			items: ['a', 'b', 'c'],
+		};
+
+		const result = sortByItemNames(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('1 greater than 2', () =>  {
+		const order1 = {
+			items: ['b', 'c', 'd'],
+		};
+
+		const order2 = {
+			items: ['a', 'b', 'c'],
+		};
+		const result = sortByItemNames(order1, order2)
+		expect(result).toBe(1)
+	})
+
+	it('1 less than 2', () =>  {
+		const order1 = {
+			items: ['a', 'b', 'c'],
+		};
+
+		const order2 = {
+			items: ['b', 'c', 'd'],
+		};
+		const result = sortByItemNames(order1, order2)
+		expect(result).toBe(-1)
+	})
+
+});
+
+describe('sortByDate function', () => {
+	it('orders\' types are not objects', () => {
+		const result = sortByDate(1, 2);
+		expect(result).toBe(0)
+	})
+
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders does not contain data', () => {
+		const order1 = {
+			m: '1',
+		};
+
+		const order2 = {
+			m: '2',
+		};
+		const result = sortByDate(order1, order2);
+		expect(result).toEqual(0);
+	});
+
+
+	it('same dates', () => {
+		const order1 = {
+			date: new Date('December 17, 1995 03:24:00'),
+		};
+
+		const order2 = {
+			date: new Date('December 17, 1995 03:24:00'),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('1 less than 2', () => {
+		const order1 = {
+			date: new Date('December 16, 1994 03:24:00'),
+		};
+
+		const order2 = {
+			date: new Date('December 17, 1995 03:24:00'),
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('1 greater than 2', () => {
+		const order1 = {
+			date: new Date('December 16, 1994 03:24:00'),
+		};
+
+		const order2 = {
+			date: new Date('December 17, 1995 03:24:00'),
+		};
+
+		const result = sortByDate(order2, order1);
+
+		expect(result).toBe(-1);
+	});
+
+});
