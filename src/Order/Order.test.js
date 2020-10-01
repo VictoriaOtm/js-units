eca222a3e0c4c7c8837e1e8fdc312e712ba18afa
MBtsky <9250499222@mail.ru>
2020-10-01 20:47:52 +0300

    Балицкий Михаил тесты

---
+++ src/Order/Order.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+//замокать getDate
+// unit на sortOrders
+// unit на компонент Order
+import { shallow, configure } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Adapter from 'enzyme-adapter-react-16'
+import Order from './Order'
+import { getDate } from '../utils/getDate'
+import {fakeOrders} from '../mock/fakeOrders'
+
+jest.mock('../utils/getDate')
+configure({ adapter : new Adapter() })
+
+
+describe('Order Component', () => {
+  
+  it('With order', () => {
+    const props = { order : fakeOrders[0] }
+    const TestOrder = <Order {...props} />;
+    const wrapper = shallow(TestOrder);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  
+  
+  it('Custom Order Components', () => {
+    const props = { order : fakeOrders[1] }
+    const TestOrder = <Order {...props} />;
+    const otherWrapper = shallow(TestOrder);
+    expect(toJson(otherWrapper)).toMatchSnapshot();
+  });
+  
+  
+  it('With order', () => {
+    const props = {
+      order : 	{
+        id: 123,
+        date: 1544356800000,
+        shop: 'Ali Express',
+      },
+    }
+    const TestOrder = <Order {...props} />;
+    const wrapper = shallow(TestOrder);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  
+  it('With order', () => {
+    const props = {}
+    const TestOrder = <Order {...props} />;
+    const wrapper = shallow(TestOrder);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
