205f25add83d49a6fdf2afb78a21e2ffbbe9b8b8
Oleg <elizarov1999@list.ru>
2020-10-01 21:09:07 +0300

    Fix Order.test.js

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -4,8 +4,10 @@ import {sortTypes} from '../utils/sortOrders';
 import {shallow, configure} from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
+
 jest.mock('../utils/getDate')
 import {getDate} from '../utils/getDate';
+import {fakeOrders} from "../mock/fakeOrders";
 
 
 beforeEach(() => {
@@ -17,20 +19,25 @@ getDate.mockReturnValue(42);
 configure({adapter: new Adapter()});
 
 describe('App component', () => {
-    const wrapper = shallow(<Order/>);
+    it('render without data', () => {
+        const wrapper = shallow(<Order/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 
-    it('render with default state DATE', () => {
+    it('render with real data', () => {
+        const wrapper = shallow(<Order order={fakeOrders[0]}/>);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
     it('set props', () => {
+        const wrapper = shallow(<Order/>);
         wrapper.setProps({order: {shop: 'sh', date: 10}});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
-    it('set null props', () => {
+    it('set null props withoud data', () => {
+        const wrapper = shallow(<Order/>);
         wrapper.setProps({order: {shop: undefined, date: undefined}});
         expect(toJson(wrapper)).toMatchSnapshot();
     });
-
 
 });
