d412db8cc7ce50f454c9856d2e964e0788d14ba2
Oleg <elizarov1999@list.ru>
2020-10-01 22:28:13 +0300

    Fix getDate func

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -10,15 +10,21 @@ import {getDate} from '../utils/getDate';
 import {fakeOrders} from "../mock/fakeOrders";
 
 
-beforeEach(() => {
-    getDate.mockClear();
-});
-
-getDate.mockReturnValue(42);
+const date = "42";
+getDate.mockReturnValue(date);
 
 configure({adapter: new Adapter()});
 
 describe('App component', () => {
+
+    beforeEach(() => {
+        getDate.mockClear();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
     it('render without data', () => {
         const wrapper = shallow(<Order/>);
         expect(toJson(wrapper)).toMatchSnapshot();
@@ -38,6 +44,11 @@ describe('App component', () => {
         const wrapper = shallow(<Order/>);
         wrapper.setProps({order: {shop: undefined, date: undefined}});
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+    it('check date mock', () => {
+        const wrapper = shallow(<Order order={fakeOrders[0]}/>);
+        let result = wrapper.find('#date').text();
+        expect(result).toEqual(date);
     });
 
 });
