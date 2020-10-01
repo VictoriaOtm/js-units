26aab7380c76d036c709a7fbb9f48b5b1f5d870a
Куклин С. И <akiraq@yandex.ru>
2020-10-01 20:59:01 +0300

    tests works fine

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -18,16 +18,16 @@ describe('Order component', () => {
         wrapper = shallow(<Order order = {fakeOrders[0]}/>);
     });
 
-    it('render with default state props', () => {
+    it('render with valid fake props', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('check date', () => {
+    it('check date mock', () => {
         let result = wrapper.find('#date').text();
         expect(result).toEqual('111');
     });
 
-    it('invalid date', () => {
+    it('invalid date mock', () => {
         getDate.mockReturnValue(null);
         wrapper = shallow(<Order order = {fakeOrders[0]}/>);
         let result = wrapper.find('#date').text();
