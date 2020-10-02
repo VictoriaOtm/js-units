a81c075ab1fd9111ad540e6976bb3bd2fc217ba5
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-10-02 15:19:09 +0300

    added gitDate mock in Order.test.js

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,12 +1,26 @@
+jest.mock("../utils/getDate"); 
+
 import React from 'react';
 import {shallow, configure} from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 import Order from './Order';
+import {getDate} from "../utils/getDate"; 
 
 configure({ adapter: new Adapter() });
+getDate.mockReturnValue("99 бредобря, десятница, год абсурда"); 
 
 describe("Object component tests", () => {
+    afterEach(() => {
+        jest.resetAllMocks()
+    });
+
+    it('component calls getDate function', () => {
+        const orderObj = {shop: "smth", date: 100, items: [1, "2", 3.0]}
+        shallow(<Order order={orderObj}/>)
+        expect(getDate).toHaveBeenCalledWith(orderObj.date)
+    });
+
     it('render with order with shop, order and items', () => {
         const wrapper = shallow(<Order order={{shop: "smth", date: 100, items: [1, "2", 3.0]}}/>);
         expect(toJson(wrapper)).toMatchSnapshot();
